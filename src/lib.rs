d4975233c08c025c5a48d60a8b97b9939b0b0197
zengpeng2018 <zengpeng2018@gmail.com>
2020-12-16 21:47:48 +0800

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+mod cache;
+mod db;
+mod config;
+mod util;
+mod mem;
+mod cache;
+mod table;
+mod level;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
